10e54a57e0a775dd07a31511053cd96e4146315a
cannblw <edgar.95.12@gmail.com>
2019-04-27 19:07:11 +0200

    Add mock method findById to profile service

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
+import { Profile } from '../models/Profile';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,13 @@ import { Injectable } from '@angular/core';
 export class ProfileService {
 
   constructor() { }
+
+  findById(id: number): Observable<Profile> {
+    // TODO: Load from service
+    return of({
+      id,
+      name: 'Edgar Chirivella',
+      birthday: '1995-03-28'
+    });
+  }
 }

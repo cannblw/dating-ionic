658b187bb1c08c7b3718be0e724c3067584be8a8
cannblw <edgar.95.12@gmail.com>
2019-05-12 16:49:39 +0200

    Add navigation to Profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Profile } from '../models/Profile';
 import { ProfileService } from '../services/profile.service';
 import { Photo } from '../models/Photo';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -10,13 +11,20 @@ import { Photo } from '../models/Photo';
 })
 export class ProfilePage implements OnInit {
 
+  id: number;
   profile: Profile;
 
+  isOwnProfile: boolean;
+
   constructor(
+    private activatedRoute: ActivatedRoute,
     private profileService: ProfileService
   ) { }
 
   ngOnInit() {
+    this.id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.isOwnProfile = this.id ? false : true;
+
     // TODO: Change id to variable from route
     this.profileService.findById(23).subscribe(profile => {
       this.profile = profile;

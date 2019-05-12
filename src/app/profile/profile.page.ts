1680913f61087aff80fe5e0b74fc93b6a06340cf
cannblw <edgar.95.12@gmail.com>
2019-05-12 12:55:23 +0200

    Refactor profile photos to service

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Profile } from '../models/Profile';
 import { ProfileService } from '../services/profile.service';
+import { Photo } from '../models/Photo';
 
 @Component({
   selector: 'app-profile',
@@ -20,6 +21,10 @@ export class ProfilePage implements OnInit {
     this.profileService.findById(23).subscribe(profile => {
       this.profile = profile;
     });
+  }
+
+  trackByPhotoId(_: number, item: Photo) {
+    return item.id;
   }
 
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -14,7 +14,18 @@ export class ProfileService {
     return of({
       id,
       name: 'Edgar Chirivella',
-      birthday: '1995-03-28'
+      birthday: '1995-03-28',
+      photos: [
+        // Do not show max-line-len errors on mock
+        /* tslint:disable */
+        { id: 1, url: 'https://previews.123rf.com/images/brickrena/brickrena1512/brickrena151200006/49127133-sonriendo-hipster-girl-in-su%C3%A9ter-de-punto-beanie-sombrero-y-con-la-taza-en-las-manos-en-el-fondo-blanc.jpg' },
+        { id: 2, url: 'https://previews.123rf.com/images/brickrena/brickrena1512/brickrena151200006/49127133-sonriendo-hipster-girl-in-su%C3%A9ter-de-punto-beanie-sombrero-y-con-la-taza-en-las-manos-en-el-fondo-blanc.jpg' },
+        { id: 3, url: 'https://previews.123rf.com/images/brickrena/brickrena1512/brickrena151200006/49127133-sonriendo-hipster-girl-in-su%C3%A9ter-de-punto-beanie-sombrero-y-con-la-taza-en-las-manos-en-el-fondo-blanc.jpg' },
+        { id: 4, url: 'https://previews.123rf.com/images/brickrena/brickrena1512/brickrena151200006/49127133-sonriendo-hipster-girl-in-su%C3%A9ter-de-punto-beanie-sombrero-y-con-la-taza-en-las-manos-en-el-fondo-blanc.jpg' },
+        { id: 5, url: 'https://previews.123rf.com/images/brickrena/brickrena1512/brickrena151200006/49127133-sonriendo-hipster-girl-in-su%C3%A9ter-de-punto-beanie-sombrero-y-con-la-taza-en-las-manos-en-el-fondo-blanc.jpg' },
+        { id: 6, url: 'https://previews.123rf.com/images/brickrena/brickrena1512/brickrena151200006/49127133-sonriendo-hipster-girl-in-su%C3%A9ter-de-punto-beanie-sombrero-y-con-la-taza-en-las-manos-en-el-fondo-blanc.jpg' }
+        /* tslint:enable */
+      ]
     });
   }
 }

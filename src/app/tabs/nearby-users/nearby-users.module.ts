dcd5a27ce0067ef8642aa092a38bafa0b3a6db57
cannblw <edgar.95.12@gmail.com>
2019-05-17 20:00:24 +0200

    Create base grid for nearby users

--- src/app/tabs/nearby-users/nearby-users.module.ts
+++ src/app/tabs/nearby-users/nearby-users.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NearbyUsersPage } from './nearby-users.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    SharedModule,
     RouterModule.forChild([{ path: '', component: NearbyUsersPage }])
   ],
   declarations: [NearbyUsersPage]
--- src/app/tabs/nearby-users/nearby-users.page.ts
+++ src/app/tabs/nearby-users/nearby-users.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Profile } from '../../shared/models/Profile';
+import { ProfileService } from 'src/app/shared/services/profile.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nearby-users',
@@ -7,6 +10,24 @@ import { Component } from '@angular/core';
 })
 export class NearbyUsersPage {
 
+  findNearbySub: Subscription;
+
+  profiles: Profile[];
+
+  constructor(
+    private profileService: ProfileService
+  ) { }
+
+  ionViewWillEnter() {
+    this.findNearbySub = this.profileService.findNearby().subscribe(profiles => {
+      this.profiles = profiles;
+    });
+  }
+
+
+  ionViewDidLeave() {
+    this.findNearbySub.unsubscribe();
+  }
 
 
 }

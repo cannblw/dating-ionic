078db2f96f8f385f91cf49a0b145261c9709188b
cannblw <edgar.95.12@gmail.com>
2019-05-22 18:27:15 +0200

    Create simple chat list view

--- src/app/tabs/chats/chats.page.ts
+++ src/app/tabs/chats/chats.page.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chats',
   templateUrl: 'chats.page.html',
   styleUrls: ['chats.page.scss']
 })
-export class ChatsPage {}
+export class ChatsPage implements OnInit {
+
+  chats: any[];
+
+  ngOnInit() {
+    this.chats = new Array(5);
+  }
+
+}

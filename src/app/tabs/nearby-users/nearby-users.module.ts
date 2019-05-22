dd7207808ab984acf0b0a3e20609954ed8167996
cannblw <edgar.95.12@gmail.com>
2019-05-22 18:15:16 +0200

    Refactor chats routes to independent routing module

--- src/app/tabs/nearby-users/nearby-users.module.ts
+++ src/app/tabs/nearby-users/nearby-users.module.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NearbyUsersPage } from './nearby-users.page';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { BrowserModule } from '@angular/platform-browser';
+import { NearbyUsersPageRoutingModule } from './nearby-users-routing.module';
 
 @NgModule({
   imports: [
@@ -13,7 +13,7 @@ import { BrowserModule } from '@angular/platform-browser';
     CommonModule,
     FormsModule,
     SharedModule,
-    RouterModule.forChild([{ path: '', component: NearbyUsersPage }])
+    NearbyUsersPageRoutingModule
   ],
   declarations: [NearbyUsersPage]
 })

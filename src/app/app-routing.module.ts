fceeb825b2aa702a0679235f1c83ae2352c06cc1
cannblw <edgar.95.12@gmail.com>
2019-05-27 23:14:12 +0200

    Fix navigation order on app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
-  { path: 'conversation', loadChildren: './conversation/conversation.module#ConversationPageModule' }
+  { path: 'conversation', loadChildren: './conversation/conversation.module#ConversationPageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
 ];
 @NgModule({
   imports: [

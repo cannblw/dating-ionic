d4b27b9cc031643b802e895859884ab2def3561d
cannblw <edgar.95.12@gmail.com>
2019-06-09 01:56:38 +0200

    Add simple layout for my-profile

--- src/app/tabs/my-profile/my-profile.page.ts
+++ src/app/tabs/my-profile/my-profile.page.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-my-profile',
   templateUrl: 'my-profile.page.html',
   styleUrls: ['my-profile.page.scss']
 })
-export class MyProfilePage {}
+export class MyProfilePage {
+
+  onReorderPhoto(event: any) {
+    const detail = event.detail as ItemReorderEventDetail;
+
+    detail.complete();
+  }
+
+  openPhotoPicker() {
+    // TODO: Open photo picker
+    console.log('Open photo picker');
+  }
+
+  openPhotoPreview() {
+    // TODO: Open photo preview
+    console.log('Open photo preview');
+  }
+
+}

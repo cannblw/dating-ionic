3b24e61774c028052f1ed450742f0c03e26f2baa
cannblw <edgar.95.12@gmail.com>
2019-06-09 18:39:15 +0200

    Add birthday and ngModels to My Profile

--- src/app/tabs/my-profile/my-profile.page.ts
+++ src/app/tabs/my-profile/my-profile.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ItemReorderEventDetail } from '@ionic/core';
+import { ConfigService } from 'src/app/shared/services/config.service';
+import { Profile } from 'src/app/shared/models/Profile';
 
 @Component({
   selector: 'app-my-profile',
@@ -7,6 +9,18 @@ import { ItemReorderEventDetail } from '@ionic/core';
   styleUrls: ['my-profile.page.scss']
 })
 export class MyProfilePage {
+
+  profile: Profile;
+  localDateFormat: string;
+
+  constructor(
+    private configService: ConfigService
+  ) { }
+
+  ionViewWillEnter() {
+    this.localDateFormat = this.configService.getLocaleDateFormat();
+    this.profile = {} as Profile;
+  }
 
   onReorderPhoto(event: any) {
     const detail = event.detail as ItemReorderEventDetail;
@@ -23,5 +37,6 @@ export class MyProfilePage {
     // TODO: Open photo preview
     console.log('Open photo preview');
   }
+
 
 }

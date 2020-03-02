8115991f7e4ca74ef2d10d1434f23938e0b2370e
cannblw <edgar.95.12@gmail.com>
2020-03-02 16:25:04 +0100

    Center img-spinner horizontally

--- src/app/shared/components/img-spinner/img-spinner.component.ts
+++ src/app/shared/components/img-spinner/img-spinner.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ImgSpinnerComponent {
 
   @Input('src') imgSrc: string;
-  @Input('class') contentContainerClass: string;
+  @Input('class') contentContainerClass = '';
 
   loading: boolean = true
 
